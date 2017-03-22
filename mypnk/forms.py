4ef500886d74dc9479408eb4db28ba0d43d855af
Rommel Rico <me@rommelrico.com>
2017-03-22 09:33:01 -0700

    Adding a TypeFilter to the PNKEmployeeAdmin form

--- mypnk/forms.py
+++ mypnk/forms.py
@@ -4,6 +4,9 @@ from .models import PNKEmployee
 
 class PNKEmployeeForm(forms.ModelForm):
 
+    def __str__(self):
+        return '%s %s' % (self.user.first_name, self.user.last_name)
+
     class Meta:
         model = PNKEmployee
         fields = '__all__'
