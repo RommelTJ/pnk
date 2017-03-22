f9a6e90b7e7c5f7819f7ccb37dac21a1c0d0e7f3
Rommel Rico <me@rommelrico.com>
2017-03-22 09:32:43 -0700

    Renaming fields in Organization and adding hire_date and type to PNKEmployee model

--- mypnk/models.py
+++ mypnk/models.py
@@ -4,19 +4,32 @@ from django.utils import timezone
 
 
 class Organization(models.Model):
-    org_name = models.CharField(unique=True, max_length=150)
-    org_short_name = models.CharField(unique=True, max_length=4, default=None)
+    name = models.CharField(unique=True, max_length=150)
+    short_name = models.CharField(unique=True, max_length=4, default=None)
     rsi_url = models.URLField(unique=True)
     website_url = models.URLField(blank=True, null=True)
     created_date = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
-        return '%s' % (self.org_name)
+        return '%s' % self.name
 
 
 class PNKEmployee(models.Model):
+
+    EMPLOYEE_TYPES = (
+        ('MEM', 'Member'),
+        ('AFF', 'Affiliate'),
+        ('RET', 'Retired'),
+        ('KIA', 'Killed in action'),
+    )
+
     user = models.ForeignKey(User)
-    org = models.ManyToManyField(Organization)
+    org = models.ManyToManyField(Organization, related_name='organizations')
+    type = models.CharField(max_length=3, choices=EMPLOYEE_TYPES, default='AFF')
+    hire_date = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
         return "%s %s" % (self.user.first_name, self.user.last_name)
+
+    class Meta:
+        db_table = 'pnk_employees'
