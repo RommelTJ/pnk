215dca745eff6f890bff2199d454529215eafbe6
Rommel Rico <me@rommelrico.com>
2017-03-22 08:24:39 -0700

    Initial PNKEmployee Model and adding short name and created_date to Organization model

--- mypnk/admin.py
+++ mypnk/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Organization
+from .models import Organization, PNKEmployee
+from .forms import PNKEmployeeForm
+
+
+class PNKEmployeeAdmin(admin.ModelAdmin):
+    form = PNKEmployeeForm
+    search_fields = ('user',)
+    list_display = ('user',)
+    save_on_top = True
+    actions_on_bottom = False
+    list_per_page = 10
+
 
 admin.site.register(Organization)
+admin.site.register(PNKEmployee, PNKEmployeeAdmin)
--- mypnk/models.py
+++ mypnk/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Organization(models.Model):
     org_name = models.CharField(unique=True, max_length=150)
+    org_short_name = models.CharField(unique=True, max_length=4, default=None)
     rsi_url = models.URLField(unique=True)
     website_url = models.URLField(blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
         return '%s' % (self.org_name)
+
+
+class PNKEmployee(models.Model):
+    user = models.ForeignKey(User)
+    org = models.ManyToManyField(Organization)
+
+    def __str__(self):
+        return "%s %s" % (self.user.first_name, self.user.last_name)
