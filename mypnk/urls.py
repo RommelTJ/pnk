eb33b3c3dfcfab6b289ff1c30fc175a6f21b1066
Rommel Rico <me@rommelrico.com>
2017-03-23 21:47:32 -0700

    A bunch of scaffolding and structure work

--- mypnk/urls.py
+++ mypnk/urls.py
@@ -17,15 +17,59 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
 
-admin.site.site_header = "PurNKleen"
+admin.site.site_header = "Pur'N'Kleen"
 admin.site.site_title = "PNK Administation"
 admin.site.index_title = "PNK"
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
-    url(r'^about/$', views.about, name='about'),
-    url(r'^jobs/$', views.jobs, name='jobs'),
-    url(r'^team/$', views.team, name='team'),
+    url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.default.urls')),
+
+    #########################
+    # Start About Views     #
+    #########################
+    url(r'^about/$', views.about, name='about'),
+    url(r'^vision/$', views.vision, name='vision'),
+    url(r'^values/$', views.values, name='values'),
+    url(r'^team/$', views.team, name='team'),
+    url(r'^benefits/$', views.benefits, name='benefits'),
+    url(r'^bylaws/$', views.bylaws, name='bylaws'),
+    url(r'^partners/$', views.partners, name='partners'),
+    url(r'^join-pnk/$', views.join_pnk, name='join-pnk'),
+    #########################
+    # End About Views       #
+    #########################
+
+    #########################
+    # Start Solutions Views #
+    #########################
+    url(r'^mission-planner/$', views.mission_planner, name='mission-planner'),
+    url(r'^fleet-view/$', views.fleet_view, name='fleet-view'),
+    #########################
+    # End Solutions Views   #
+    #########################
+
+    #########################
+    # Start Services Views  #
+    #########################
+    url(r'^fuel-services/$', views.fuel_services, name='fuel-services'),
+    url(r'^maintenance-and-repair/$', views.maintenance_repair, name='maintenance-and-repair'),
+    url(r'^transportation/$', views.transportation, name='transportation'),
+    url(r'^implementation/$', views.implementation, name='implementation'),
+    url(r'^support-and-training/$', views.support_training, name='support-and-training'),
+    url(r'^consulting-services/$', views.consulting_services, name='consulting-services'),
+    url(r'^professional-services/$', views.professional_services, name='professional-services'),
+    #########################
+    # End Services Views    #
+    #########################
+
+    #########################
+    # Start Knowledge Views #
+    #########################
+    url(r'^pnk-podcast/$', views.pnk_podcast, name='pnk-podcast'),
+    url(r'^links-and-tools/$', views.links_tools, name='links-and-tools'),
+    #########################
+    # End Knowledge Views   #
+    #########################
 ]
--- mypnk/views.py
+++ mypnk/views.py
@@ -5,13 +5,101 @@ def index(request):
     return render(request, 'index.html', {})
 
 
+#########################
+# Start About Views     #
+#########################
 def about(request):
     return render(request, 'about.html', {})
 
 
-def jobs(request):
-    return render(request, 'jobs.html', {})
+def vision(request):
+    return render(request, 'vision.html', {})
+
+
+def values(request):
+    return render(request, 'values.html', {})
 
 
 def team(request):
     return render(request, 'team.html', {})
+
+
+def benefits(request):
+    return render(request, 'benefits.html', {})
+
+
+def bylaws(request):
+    return render(request, 'bylaws.html', {})
+
+
+def partners(request):
+    return render(request, 'partners.html', {})
+
+
+def join_pnk(request):
+    return render(request, 'join-pnk.html', {})
+#########################
+# End About Views       #
+#########################
+
+
+#########################
+# Start Solutions Views #
+#########################
+def mission_planner(request):
+    return render(request, 'mission-planner.html', {})
+
+
+def fleet_view(request):
+    return render(request, 'fleet-view.html', {})
+#########################
+# End Solutions Views   #
+#########################
+
+
+#########################
+# Start Services Views  #
+#########################
+def fuel_services(request):
+    return render(request, 'fuel-services.html', {})
+
+
+def maintenance_repair(request):
+    return render(request, 'maintenance-and-repair.html', {})
+
+
+def transportation(request):
+    return render(request, 'transportation.html', {})
+
+
+def implementation(request):
+    return render(request, 'implementation.html', {})
+
+
+def support_training(request):
+    return render(request, 'support-and-training.html', {})
+
+
+def consulting_services(request):
+    return render(request, 'consulting-services.html', {})
+
+
+def professional_services(request):
+    return render(request, 'professional-services.html', {})
+#########################
+# End Services Views    #
+#########################
+
+
+#########################
+# Start Knowledge Views #
+#########################
+def pnk_podcast(request):
+    return render(request, 'pnk-podcast.html', {})
+
+
+def links_tools(request):
+    return render(request, 'links-and-tools.html', {})
+#########################
+# End Knowledge Views   #
+#########################
