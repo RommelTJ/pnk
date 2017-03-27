2169dbe3a4022e96571bbb4355d4ba4677a1103a
Rommel Rico <me@rommelrico.com>
2017-03-26 22:05:55 -0700

    Allowed hosts

--- mypnk/settings.py
+++ mypnk/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config.get('key', 'SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.purnkleen.com', 'purnkleen.com', '127.0.0.1']
 
 EMAIL_HOST = config.get('email', 'EMAIL_HOST')
 EMAIL_HOST_USER = config.get('email', 'EMAIL_HOST_USER')
