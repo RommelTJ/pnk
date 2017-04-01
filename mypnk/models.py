9aa836a2afb021ed9af89716279b88148dba6ec3
Rommel Rico <me@rommelrico.com>
2017-04-01 16:51:23 -0700

    Adding RSI URL to the PNKEmployee model

--- mypnk/models.py
+++ mypnk/models.py
@@ -59,6 +59,7 @@ class PNKEmployee(models.Model):
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='M')
     type = models.CharField(max_length=3, choices=EMPLOYEE_TYPES, default='AFF')
     callsign = models.CharField(max_length=255)
+    rsi_url = models.URLField(default='https://robertsspaceindustries.com/citizens/rommelrico')
     birth_date = models.DateField(default=datetime(1980, 01, 01))
     hire_date = models.DateTimeField(default=timezone.now, blank=True)
     image = StdImageField(upload_to=upload_path_handler, storage=fs, null=True, blank=True, max_length=255, variations={
